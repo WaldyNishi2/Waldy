65a0fb473487f659ede46b1344ae3eaef9f0ab18
WaldyNishi2 <138442977+WaldyNishi2@users.noreply.github.com>
2023-07-04 12:01:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,8 +32,8 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://WALDY:WALDYY@cluster0.7at5zew.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Waldy")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Waldyfiles")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_filess')
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001405818976'))
